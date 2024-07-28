ebe34fee54112c9665d062b32abe445eebf48f48
vhawk19 <malavikamenon2000@gmail.com>
2024-07-28 19:27:21 +0530

    change icon position

--- src/components/DataCatalog.vue
+++ src/components/DataCatalog.vue
@@ -16,7 +16,7 @@
                    file:border-0
                    file:text-sm file:font-semibold
                    file:bg-blue-50 file:text-green-700
-                   hover:file:bg-green-100"
+                   hover:file:bg-green-100 rounded"
           />
         </label>
         <!-- <button
--- src/components/QueryWorkspace.vue
+++ src/components/QueryWorkspace.vue
@@ -2,12 +2,12 @@
     <div class="query-workspace flex h-screen">
       <div class="flex-grow overflow-y-auto p-6">
         <h1 class="text-2xl font-bold mb-4 p-6">Query Workspace</h1>
-        <button @click="addQueryBlock" class="bg-white text-green-600 font-bold py-2 px-6 mx-6 rounded mb-4">
-          Add Query Block
-        </button>
         <div v-for="block in queryBlocks" :key="block.id" class="mb-4">
           <QueryBlock :blockId="block.id" />
         </div>
+        <button @click="addQueryBlock" class="bg-white text-green-600 font-bold py-2 px-6 mx-6 rounded mb-4">
+          Add Query Block
+        </button>
       </div>
     </div>
   </template>
