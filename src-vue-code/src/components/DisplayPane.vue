e33edf7d38c535bfa81941da97be0ac06e276ab8
vhawk19 <varunhawk19@gmail.com>
2024-07-28 15:54:52 +0530

    wip: idk at this point

--- src-vue-code/src/components/DisplayPane.vue
+++ src-vue-code/src/components/DisplayPane.vue
@@ -10,15 +10,16 @@
         <div v-show="!isCollapsed" class="content">
           <div 
             id="displayArea" 
-            @dragover.prevent 
-            @drop="drop" 
             ref="displayArea"
             class="min-h-[200px] border-2 border-dashed border-gray-600 rounded-lg p-4 mb-4"
           >
-            <p v-if="isEmpty" class="text-gray-400 text-center">Drag visualizations here</p>
+            <p v-if="isEmpty" class="text-gray-400 text-center">Add visualizations here</p>
+            <div v-for="item in displayItems" :key="item.id" class="mb-4">
+              <h3 class="text-lg font-semibold mb-2">{{ item.name }}</h3>
+              <div :id="`${item.id}-container`" class="bg-gray-700 p-4 rounded-lg"></div>
+            </div>
           </div>
           <button 
-            id="exportPdfBtn" 
             @click="exportToPdf"
             class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full"
           >
@@ -29,62 +30,49 @@
     </div>
   </template>
   
-  <script>
-  import { createVisualization } from "../visualization"
+  <script setup>
+  import { ref, computed, onMounted, watch } from 'vue'
+  import { useDisplayPaneStore } from '@/stores/displayPaneStore'
+  import { createVisualization } from "@/utils/visualization"
   
-  export default {
-      name: 'DisplayPane',
-      data() {
-          return {
-              isCollapsed: false,
-              isEmpty: true,
-              draggedElement: null
-          }
-      },
-      methods: {
-          toggleCollapse() {
-              this.isCollapsed = !this.isCollapsed
-          },
-          createDraggableVisualization(blockId, vizId) {
-              const vizElement = document.createElement('div')
-              vizElement.id = `draggable-${vizId}`
-              vizElement.className = 'draggable-viz bg-gray-700 p-4 rounded-lg mb-4'
-              vizElement.draggable = true
-              vizElement.innerHTML = `
-                  <h3 class="text-lg font-semibold mb-2">Visualization ${vizId.split('-')[1]}</h3>
-                  <div id="${vizId}-display-container" class="bg-gray-800 p-2 rounded"></div>
-              `
+  const displayPaneStore = useDisplayPaneStore()
+  const isCollapsed = ref(false)
+  const displayArea = ref(null)
   
-              vizElement.addEventListener('dragstart', this.dragStart)
+  const displayItems = computed(() => displayPaneStore.items)
+  const isEmpty = computed(() => displayItems.value.length === 0)
   
-              const data = JSON.parse(sessionStorage.getItem(`${blockId}-data`) || '[]')
-              const chartType = document.getElementById(`${vizId}-chart-type`).value
-              const xAxis = document.getElementById(`${vizId}-x-axis`).value
-              const yAxis = document.getElementById(`${vizId}-y-axis`).value
-  
-              const container = vizElement.querySelector(`#${vizId}-display-container`)
-              createVisualization(container, data, chartType, xAxis, yAxis)
-  
-              return vizElement
-          },
-          dragStart(e) {
-              this.draggedElement = e.target
-              e.dataTransfer.setData('text/plain', e.target.id)
-          },
-          drop(e) {
-              e.preventDefault()
-              const data = e.dataTransfer.getData('text')
-              const draggedElement = document.getElementById(data)
-              if (this.$refs.displayArea.innerHTML === '<p class="text-gray-400 text-center">Drag visualizations here</p>') {
-                  this.$refs.displayArea.innerHTML = ''
-              }
-              this.$refs.displayArea.appendChild(draggedElement)
-              this.isEmpty = false
-          },
-          exportToPdf() {
-              console.log('Exporting to PDF...')
-              alert('PDF export functionality to be implemented')
-          }
-      }
+  function toggleCollapse() {
+    isCollapsed.value = !isCollapsed.value
   }
+  
+  function exportToPdf() {
+    console.log('Exporting to PDF...')
+    alert('PDF export functionality to be implemented')
+  }
+  
+  watch(displayItems, () => {
+    nextTick(() => {
+      displayItems.value.forEach(item => {
+        if (item.type === 'visualization') {
+          const container = document.getElementById(`${item.id}-container`)
+          if (container) {
+            createVisualization(container, item.data, item.chartType, item.xAxis, item.yAxis)
+          }
+        }
+      })
+    })
+  }, { deep: true })
+  
+  onMounted(() => {
+    // Initial render of visualizations
+    displayItems.value.forEach(item => {
+      if (item.type === 'visualization') {
+        const container = document.getElementById(`${item.id}-container`)
+        if (container) {
+          createVisualization(container, item.data, item.chartType, item.xAxis, item.yAxis)
+        }
+      }
+    })
+  })
   </script>
