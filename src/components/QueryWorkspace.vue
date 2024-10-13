6e824fe8335d37ba8bb957cad05a2291aa98859f
malavikasmenon <malavikamenon2000@gmail.com>
2024-10-13 21:33:37 +0530

    add icons, change button color

--- src/components/QueryWorkspace.vue
+++ src/components/QueryWorkspace.vue
@@ -1,26 +1,29 @@
 <template>
-    <div class="query-workspace flex h-screen">
-      <div class="flex-grow overflow-y-auto p-6">
-        <h1 class="text-2xl font-bold mb-4 p-6">Query Workspace</h1>
-        <div v-for="block in queryBlocks" :key="block.id" class="mb-4">
-          <QueryBlock :blockId="block.id" />
-        </div>
-        <button @click="addQueryBlock" class="bg-white text-green-600 font-bold py-2 px-6 mx-6 rounded mb-4">
-          Add Query Block
-        </button>
+  <div class="query-workspace flex h-screen">
+    <div class="flex-grow overflow-y-auto p-6">
+      <h1 class="text-2xl font-bold mb-4 p-6">Query Workspace</h1>
+      <div v-for="block in queryBlocks" :key="block.id" class="mb-4">
+        <QueryBlock :blockId="block.id" />
       </div>
+      <button
+        @click="addQueryBlock"
+        class="text-white-600 font-bold py-2 px-6 mx-6 rounded mb-4 border-white border-solid border-2"
+      >
+        Add Query Block
+      </button>
     </div>
-  </template>
-  
-  <script setup>
-  import { onMounted } from 'vue'
-  import QueryBlock from '@/components/QueryBlock.vue'
-  import { useQueryBlockStore } from '@/stores/queryBlockStore'
-  
-  const queryBlockStore = useQueryBlockStore()
-  const { queryBlocks, addQueryBlock } = queryBlockStore
-  
-  onMounted(() => {
-    addQueryBlock()
-  })
-  </script>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import QueryBlock from "@/components/QueryBlock.vue";
+import { useQueryBlockStore } from "@/stores/queryBlockStore";
+
+const queryBlockStore = useQueryBlockStore();
+const { queryBlocks, addQueryBlock } = queryBlockStore;
+
+onMounted(() => {
+  addQueryBlock();
+});
+</script>
--- src/main.js
+++ src/main.js
@@ -6,10 +6,23 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import {faPlay, faSquarePlus, faTrash } from '@fortawesome/free-solid-svg-icons'
+// import { faSquarePlus } from '@fortawesome/free-regular-svg-icons'
+
+/* add icons to the library */
+library.add(faPlay, faTrash, faSquarePlus)
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
